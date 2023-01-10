2b7175ca89382598f127dbb45b8a94a9441dd0aa
Joshua <joshuagutierrez724@gmail.com>
2023-01-09 21:55:59 -0500

    Messed up the launch icon while putting in image assets. Gonna have to look it up again.

--- app/src/main/java/com/example/recyclerview2/MainActivity.java
+++ app/src/main/java/com/example/recyclerview2/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
+//We are gonna slap a bunch of models into an arrayList.
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<PigModel> pigList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void setupPig(){
+
     }
 }
---
+++ app/src/main/java/com/example/recyclerview2/PigModel.java
@@ -0,0 +1,19 @@
+package com.example.recyclerview2;
+
+public class PigModel {
+    private String name;
+    private int image;
+
+    public PigModel(String name, int image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
