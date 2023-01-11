ff546878cb523f84c4ffaa9d515fc0c04a9ab7f5
Joshua <joshuagutierrez724@gmail.com>
2023-01-10 20:56:27 -0500

    Pig pictures got added in. I'll fix the icon later

--- app/src/main/java/com/example/recyclerview2/MainActivity.java
+++ app/src/main/java/com/example/recyclerview2/MainActivity.java
@@ -11,13 +11,23 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<PigModel> pigList = new ArrayList<>();
 
+    int[] pigPicture = {R.drawable.southern_pig, R.drawable.city_pig, R.drawable.resort_pig};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupPig();
     }
 
     private void setupPig(){
+        //Gotta get back our string array
+        String[] pigName = getResources().getStringArray(R.array.pig_full_text);
 
+        for(int i = 0; i< pigName.length; i++){
+            //Array of pig objects must be filled in
+            pigList.add(new PigModel(pigName[i], pigPicture[i]));
+        }
     }
 }
